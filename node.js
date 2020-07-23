9bbf48590091a10203fedf4fda96157195c14e50
Maycon Sousa <mayconfsousa@gmail.com>
2020-07-23 10:19:46 -0300

    fix: disable some rules

--- node.js
+++ node.js
@@ -27,5 +27,6 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
   },
 };
--- react.js
+++ react.js
@@ -37,6 +37,7 @@ module.exports = {
     '@typescript-eslint/camelcase': 'off',
     '@typescript-eslint/explicit-function-return-type': 'off',
     '@typescript-eslint/explicit-module-boundary-types': 'off',
+    '@typescript-eslint/no-var-requires': 'off',
     'react/display-name': 'off',
     'react/prop-types': 'off',
   },
